44e2354cf639d8121697376dfe3069822b39e0c1
Alejandro Flores Martinez <alexfire94@outlook.com>
2017-03-20 04:22:54 -0600

    Primer Commit

---
+++ backend/views/layouts/main.php
@@ -0,0 +1,85 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use backend\assets\AppAsset;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use common\widgets\Alert;
+use Yii\helpers\Url;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Yii::$app->name ?></title>
+    <?php $this->head() ?>
+    <link rel="shortcut icon" href="/favicon.ico?v=2" type="image/x-icon" />
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <?php   
+    NavBar::begin([
+        'brandLabel' => Yii::$app->name,
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
+    $menuItems = [
+        ['label' => 'Inicio', 'url' => ['/site/index'], 'visible'=> !Yii::$app->user->isGuest],
+        ['label' => 'Mi perfil', 'url' => ['user/view'], 'visible'=> !Yii::$app->user->isGuest],
+
+    ];
+    if (Yii::$app->user->isGuest) {
+        $menuItems[] = ['label' => 'Iniciar Sesion', 'url' => ['/site/login']];
+    } else {
+        $menuItems[] = '<li>'
+            . Html::beginForm(['/site/logout'], 'post')
+            . Html::submitButton(
+                'Cerrar Sesion (' . Yii::$app->user->identity->username . ')',
+                ['class' => 'btn btn-link logout']
+            )
+            . Html::endForm()
+                        
+            . '</li>';
+
+    }
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => $menuItems,
+    ]);
+    NavBar::end();
+    ?>
+
+    <div class="container">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= Alert::widget() ?>
+        <?= $content ?>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; <?= Yii::$app->name, date('Y') ?></p>
+
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
---
+++ backend/views/site/error.php
@@ -0,0 +1,27 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $name string */
+/* @var $message string */
+/* @var $exception Exception */
+
+use yii\helpers\Html;
+
+$this->title = $name;
+?>
+<div class="site-error">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div class="alert alert-danger">
+        <?= nl2br(Html::encode($message)) ?>
+    </div>
+
+    <p>
+        El error anterior ocurrio cuando el servidor web estaba procesando tu petición.
+    </p>
+    <p>
+        Por favor contactanos si crees que esto es un error del servidor. Gracias.
+    </p>
+
+</div>
