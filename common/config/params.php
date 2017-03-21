0c0bbb2f039c50db75e0d5f8f7b8ce08eb4ab5ae
Alejandro Flores Martinez <alexfire94@outlook.com>
2017-03-20 22:15:47 -0600

    Agregado correo electronico del administrador.

--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
+    'adminEmail' => 'alexfire94@outlook.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
 ];
