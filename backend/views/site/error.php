fece1a1e14cf645f0449adfb847910e2c11e5d0b
Alejandro Flores Martinez <alexfire94@outlook.com>
2017-03-23 01:57:08 -0600

    Layouts editados de adminlte

--- backend/views/site/error.php
+++ backend/views/site/error.php
@@ -1,27 +1,43 @@
 <?php
 
+use yii\helpers\Html;
+
 /* @var $this yii\web\View */
 /* @var $name string */
 /* @var $message string */
 /* @var $exception Exception */
 
-use yii\helpers\Html;
-
 $this->title = $name;
 ?>
-<div class="site-error">
+<section class="content">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="error-page">
+        <h2 class="headline text-info"><i class="fa fa-warning text-yellow"></i></h2>
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+        <div class="error-content">
+            <h3><?= $name ?></h3>
+
+            <p>
+                <?= nl2br(Html::encode($message)) ?>
+            </p>
+
+            <p>
+                El error anterior ocurrio cuando el servidor web estaba procesando tu petición.
+                Por favor contactanos si crees que esto es un error del servidor. Gracias.
+                Meanwhile, you may <a href='<?= Yii::$app->homeUrl ?>'>return to dashboard</a>
+            </p>
+<!--
+            <form class='search-form'>
+                <div class='input-group'>
+                    <input type="text" name="search" class='form-control' placeholder="Search"/>
+
+                    <div class="input-group-btn">
+                        <button type="submit" name="submit" class="btn btn-primary"><i class="fa fa-search"></i>
+                        </button>
+                    </div>
+                </div>
+            </form> -->
+        </div>
     </div>
 
-    <p>
-        El error anterior ocurrio cuando el servidor web estaba procesando tu petición.
-    </p>
-    <p>
-        Por favor contactanos si crees que esto es un error del servidor. Gracias.
-    </p>
-
-</div>
+</section>
