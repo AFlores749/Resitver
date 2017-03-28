6449a7cfad96e715d6f57031533dc5de0c95cb7e
Alejandro Flores Martinez <alexfire94@outlook.com>
2017-03-27 21:44:36 -0600

    Texto personalizado

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -5,16 +5,16 @@
         <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
+                <img src="<?= Yii::$app->user->identity->Img_Perfil?>" class="img-circle" alt="User Image"/>
             </div>
             <div class="pull-left info">
-                <p>Alexander Pierce</p>
+                <p><?= Yii::$app->user->identity->Nombre_Completo?></p>
 
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+                <!--<a href="#"><i class="fa fa-circle text-success"></i> Online</a> -->
             </div>
         </div> -
 
-        <!-- search form -->
+        <!-- search form --><!--
         <form action="#" method="get" class="sidebar-form">
             <div class="input-group">
                 <input type="text" name="q" class="form-control" placeholder="Buscar..."/>
@@ -23,46 +23,19 @@
                 </button>
               </span>
             </div>
-        </form>
-        <!-- /.search form -->
+        </form> -->
+        <!-- /.search form -->  
 
         <?= dmstr\widgets\Menu::widget(
             [
                 'options' => ['class' => 'sidebar-menu'],
                 'items' => [
-                  //  ['label' => 'Menu', 'options' => ['class' => 'header']],
+                    ['label' => 'Menu', 'options' => ['class' => 'header']],
                     ['label' => 'Inicio', 'url' => ['/site/index']],
                     ['label' => 'Mi perfil', 'url' => ['/user/view']],
-                  /*  ['label' => 'Gii', 'icon' => 'fa fa-file-code-o', 'url' => ['/gii']],
-                    ['label' => 'Debug', 'icon' => 'fa fa-dashboard', 'url' => ['/debug']],
-                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
-                    [
-                        'label' => 'Same tools',
-                        'icon' => 'fa fa-share',
-                        'url' => '#',
-                        'items' => [
-                            ['label' => 'Gii', 'icon' => 'fa fa-file-code-o', 'url' => ['/gii'],],
-                            ['label' => 'Debug', 'icon' => 'fa fa-dashboard', 'url' => ['/debug'],],
-                            [
-                                'label' => 'Level One',
-                                'icon' => 'fa fa-circle-o',
-                                'url' => '#',
-                                'items' => [
-                                    ['label' => 'Level Two', 'icon' => 'fa fa-circle-o', 'url' => '#',],
-                                    [
-                                        'label' => 'Level Two',
-                                        'icon' => 'fa fa-circle-o',
-                                        'url' => '#',
-                                        'items' => [
-                                            ['label' => 'Level Three', 'icon' => 'fa fa-circle-o', 'url' => '#',],
-                                            ['label' => 'Level Three', 'icon' => 'fa fa-circle-o', 'url' => '#',],
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
-                */],
+                    ['label' => 'Mi calendario', 'url' => ['/site/index']],
+                    ['label' => 'Mis documentos', 'url' => ['/site/index']],
+                  ],
             ]
         ) ?>
 
